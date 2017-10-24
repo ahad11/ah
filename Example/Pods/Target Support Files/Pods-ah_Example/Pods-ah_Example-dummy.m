6155918c60be3242f8d9e3edbdb476a769cea7d1
ahad375 <aalarifi64@gmail.com>
2017-10-24 11:07:05 +0300

    f

---
+++ Example/Pods/Target Support Files/Pods-ah_Example/Pods-ah_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ah_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ah_Example
+@end
---
+++ Example/Pods/Target Support Files/ah/ah-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ah : NSObject
+@end
+@implementation PodsDummy_ah
+@end
